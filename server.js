cf688455130920ec9671ef6a67a0d4ee990cbd8a
luana <luanaarrudasabara06@gmail.com>
2026-04-10 13:58:07 -0300

    conectando banco railway

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ function connectDatabase() {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
   db.connect((err) => {
