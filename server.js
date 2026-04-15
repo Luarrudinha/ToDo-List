9102884386626b41f511a7775858ba4e3d34af7c
luana <luanaarrudasabara06@gmail.com>
2026-04-15 10:09:48 -0300

    corrige dockerfile para deploy

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // --- Configuração do Banco de Dados ---
 function connectDatabase() {
